e03b144a8ca97ab4b601de05eae7c4ffb636e3d3
Wahid Halim <wahidkeo@gmail.com>
2025-12-22 10:32:28 +0430

    feat: update UserBio component to display user details and join date

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
   fullWidth?: boolean;
   large?: boolean;
   onClick?: () => void;
-  disabled: boolean;
+  disabled?: boolean;
   outline?: boolean;
 }
 
--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -3,6 +3,7 @@ import { useCurrentUser } from "@/hooks/useCurrentUser";
 import { useUser } from "@/hooks/useUser";
 import React, { useMemo } from "react";
 import Button from "../Button";
+import { BiCalendar } from "react-icons/bi";
 
 interface UserBioProps {
   userId: string;
@@ -33,6 +34,29 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         ) : (
           <Button secondary label="Follow" />
         )}
+      </div>
+      <div className="mt-8 px-4">
+        <div className="flex flex-col">
+          <p className="text-white text-2xl font-semibold">{user?.name}</p>
+          <p className="text-md text-neutral-500">@{user.username}</p>
+        </div>
+        <div className="flex flex-col mt-4">
+          <p className="text-white">{user?.bio}</p>
+          <div className="flex flex-row items-center gap-2 mt-4 text-neutral-500">
+            <BiCalendar size={24} />
+            <p>Joined {createdAt}</p>
+          </div>
+        </div>
+        <div className="flex flex-row items-center mt-3 gap-6">
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{user?.followingIds?.length}</p>
+            <p className="text-neutral-500">Following</p>
+          </div>
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{user?.followersCount || 0}</p>
+            <p className="text-neutral-500">Followers</p>
+          </div>
+        </div>
       </div>
     </div>
   );
